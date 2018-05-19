5e276163a373c44c6c67bfb4fa51f87e12d5f23b
Dor Hanegby <dorhanegbyy@gmail.com>
2018-05-19 17:13:22 +0300

    last commit

--- HomeWork5/MainHW5.java
+++ HomeWork5/MainHW5.java
@@ -13,16 +13,26 @@ import weka.classifiers.functions.supportVector.PolyKernel;
 import weka.classifiers.functions.supportVector.RBFKernel;
 
 class KernelResult {
+	private Kernel kernelType;
 	private double kernelParameter;
 	private double power;
 	private double TPR;
 	private double FPR;
 
-	public KernelResult(double kernelParameter, double power, double TPR, double FPR) {
+	public KernelResult(double kernelParameter, double power, double TPR, double FPR, Kernel kernelType) {
+		this.kernelType = kernelType;
 		this.kernelParameter = kernelParameter;
 		this.power = power;
 		this.TPR = TPR;
 		this.FPR = FPR;
+	}
+
+	public Kernel getKernelType() {
+		return kernelType;
+	}
+
+	public void setKernelType(Kernel kernelType) {
+		this.kernelType = kernelType;
 	}
 
 	public double getPower() {
@@ -56,6 +66,11 @@ class KernelResult {
 	public void setFPR(double FPR) {
 		this.FPR = FPR;
 	}
+
+	public enum Kernel {
+		RBF,
+		POLY
+	}
 }
 
 public class MainHW5 {
@@ -81,8 +96,9 @@ public class MainHW5 {
 	
 	public static void main(String[] args) throws Exception {
 
-		Instances trainingData = loadData("HomeWork5/Data/training.arff");
-		Instances testData = loadData("HomeWork5/Data/test.arff");
+		Instances data = loadData("HomeWork5/Data/cancer.arff");
+		Instances trainingData = data.trainCV(5, 1);
+		Instances testData = data.testCV(5, 1);
 
 		SVM svm = new SVM();
 		double alpha = 1.5;
@@ -93,11 +109,47 @@ public class MainHW5 {
 		if (maxPoly.getPower() > maxRBF.getPower()) {
 			System.out.println("The best kernel is PolyKernel with degree " + maxPoly.getKernelParameter()
 					+ " rates: " + maxPoly.getTPR() + " / " + maxPoly.getFPR());
+			slackValueMaximize(svm, maxPoly, trainingData, testData);
 		} else {
 			System.out.println("The best kernel is RBFKernel with gamma " + maxRBF.getKernelParameter()
 					+ " rates : " + maxRBF.getTPR() + " / " + maxPoly.getFPR());
+			slackValueMaximize(svm, maxRBF, trainingData, testData);
 		}
 
+	}
+
+	private static void slackValueMaximize(SVM svm, KernelResult bestKernel, Instances trainingData, Instances testData) throws Exception{
+		int[] confusions;
+		double TPR = 0.0;
+		double FPR = 0.0;
+		KernelResult.Kernel kernelType = bestKernel.getKernelType();
+		if(kernelType == KernelResult.Kernel.POLY) {
+			PolyKernel polyKernel = new PolyKernel();
+			polyKernel.setExponent(bestKernel.getKernelParameter());
+			svm.setKernel(polyKernel);
+		}
+		else {
+			RBFKernel rbfKernel = new RBFKernel();
+			rbfKernel.setGamma(bestKernel.getKernelParameter());
+			svm.setKernel(rbfKernel);
+		}
+
+		int[] exponentValues = new int[] {1, 0 , -1, -2, -3, -4};
+		int[] linearValues = new int[] {3, 2 ,1 };
+
+		for (int i : exponentValues) {
+			for (int j: linearValues) {
+				double c = Math.pow(10, i) * ((double) j / 3);
+				svm.setC(c);
+				svm.buildClassifier(trainingData);
+				confusions = svm.calcConfusion(testData);
+				TPR = getTPR(confusions);
+				FPR = getFPR(confusions);
+				System.out.println("For C " + c + " the rates are: ");
+				printRates(TPR, FPR);
+				System.out.println();
+			}
+		}
 	}
 
 	private static KernelResult rbfKernelMaximize(SVM svm, Instances trainingData, Instances testData, double alpha) throws Exception {
@@ -123,6 +175,7 @@ public class MainHW5 {
 
 			System.out.println("For RBFKernel with gamma " + gamma + " the rates are: ");
 			printRates(TPR, FPR);
+			System.out.println();
 
 			double power = alpha * TPR - FPR;
 			if (power > maxPower) {
@@ -131,7 +184,7 @@ public class MainHW5 {
 			}
 		}
 
-		return new KernelResult(maxGamma, maxPower, TPR, FPR);
+		return new KernelResult(maxGamma, maxPower, TPR, FPR, KernelResult.Kernel.RBF);
 	}
 
 	private static KernelResult polyKernelMaximize(SVM svm, Instances trainingData, Instances testData, double alpha) throws Exception {
@@ -155,7 +208,7 @@ public class MainHW5 {
 
 			System.out.println("For PolyKernel with degree " + deg + " the rates are: ");
 			printRates(TPR, FPR);
-
+			System.out.println();
 			double power = alpha * TPR - FPR;
 			if (power > maxPower) {
 				maxPower = power;
@@ -163,7 +216,7 @@ public class MainHW5 {
 			}
 		}
 
-		return new KernelResult(maxDeg, maxPower, TPR, FPR);
+		return new KernelResult(maxDeg, maxPower, TPR, FPR, KernelResult.Kernel.POLY);
 
 	}
 
